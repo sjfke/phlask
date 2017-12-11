de7844405409d7adc53af248f81819f9cfde45f1
Geoff Collis <gcollis@ymail.com>
2017-12-11 17:08:39 +0100

    updates

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 # config.py
 
+# But in DEBUG mode flask binds only to 127.0.0.1
+# Ignore the app.run(host='0.0.0.0' port=8080) option doesn't work in DEBUG mode.
+# $ export FLASK_APP=run.py; flask run --host='0.0.0.0' # to over-ride
+#
 # Enable Flask's debugging features. Should be False in production
 DEBUG = True
